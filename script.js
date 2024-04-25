0a92587a808bae0519e6b7081e581567d4a1f23a
ravindu <ravindulakshitha250@gmail.com>
2024-04-25 08:53:48 +0530

    "ckecked" class add for script

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function addTask(){
 
 listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
+        e.target.classList.toggle("ckecked");
         saveData();
     }
     else if (e.target.tagName === "SPAN"){
